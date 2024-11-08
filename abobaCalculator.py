e21ab46f241a2e26f2e4b4197cadfb58627bfbe1
gywe_gyh <ivan.maks0130@gmail.com>
2024-11-08 16:41:39 +0300

    Add files via upload

---
+++ abobaCalculator.py
@@ -0,0 +1,15 @@
+#Aboba Calculator
+
+aboba = int(input("type a number:"))
+Isabeme = True
+if Isabeme== True:
+    print("Aboba Is Aboba")
+if aboba == 7:
+    print("7 это 6 интервалов")
+if aboba  > 8:
+    print("Куда разогнался")
+
+    #Калькулятор:
+num1 = int(input())
+num2 = int(input())
+print(num1 + num2)
